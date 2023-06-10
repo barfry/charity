c9d285298294eb16180e726e9911e99793dd03d3
barfry <bartlomiej.frylow@gmail.com>
2023-06-10 12:00:51 +0200

    Role entity and repository

---
+++ src/main/java/pl/coderslab/charity/model/Role.java
@@ -0,0 +1,44 @@
+package pl.coderslab.charity.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank(message = "This field can't be empty")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Role() {
+    }
+}
--- src/main/java/pl/coderslab/charity/service/UserService.java
+++ src/main/java/pl/coderslab/charity/service/UserService.java
@@ -2,6 +2,7 @@ package pl.coderslab.charity.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.coderslab.charity.repository.RoleRepository;
 import pl.coderslab.charity.repository.UserRepository;
 
 @Service
